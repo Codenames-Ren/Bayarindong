076c07af52abe3da5e11dc34bebf1a1ec4433b13
Codenames-Ren <renmizuki38@gmail.com>
2025-07-30 20:54:55 +0700

    update models

--- src/models/models.go
+++ src/models/models.go
@@ -17,3 +17,14 @@ type User struct {
 	UpdatedAt			time.Time
 	DeletedAt			gorm.DeletedAt		`gorm:"index"`
 }
+
+type OTP struct {
+	ID 				uint	 	`gorm:"primaryKey"`
+	UserID 			string 		`gorm:"not null"`
+	User 			User 		`gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE"`
+	Code 			string 		`gorm:"not null"`
+	ExpiresAt 		time.Time 	`gorm:"not null"`
+	Purpose 		string		`gorm:"not null"`
+	Used			bool		`gorm:"default:false"`
+	CreatedAt 		time.Time
+}
